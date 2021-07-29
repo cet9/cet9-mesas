21088c14674a5158ed28eb9b14446d9581481779
cet9 <cet9dev@gmail.com>
2021-07-29 09:34:17 -0300

    Subo formulario mesa

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <Menu/>
   <HelloWorld msg="Inscripción a mesas"/>
-  
+  <Mesa/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Menu from './components/Menu.vue'
+import Mesa from './components/Mesa.vue'
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Menu
+    Menu,
+    Mesa
   }
 }
 </script>
