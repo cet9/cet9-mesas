dbd37cce2d641a236cf7c3259a2e037919f05844
cet9.dev <cet9dev@gmail.com>
2021-07-29 10:36:23 -0300

    correcion

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,15 @@
 <template>
   <Menu/>
-<<<<<<< HEAD
-  <HelloWorld msg="Inscripción a mesas"/>
-  <Mesa/>
-  <Inscripcion/>
-=======
   <router-view/>
->>>>>>> c3ce87621c6a06408b317c6c139dc86cb4a791f6
 </template>
 <script>
-<<<<<<< HEAD
-import HelloWorld from './components/HelloWorld.vue'
-import Menu from './components/Menu.vue'
-import Mesa from './components/Mesa.vue'
-import Inscripcion from './components/Inscripcion.vue'
-=======
 // @ is an alias to /src
 import Menu from '@/components/Menu.vue'
 
->>>>>>> c3ce87621c6a06408b317c6c139dc86cb4a791f6
 export default {
 
   components: {
-<<<<<<< HEAD
-    HelloWorld,
-    Menu,
-    Mesa,
-    Inscripcion
-=======
     Menu
->>>>>>> c3ce87621c6a06408b317c6c139dc86cb4a791f6
   }
 }
 </script>
