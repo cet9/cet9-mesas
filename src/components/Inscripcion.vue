26023182e0f7dc27f245f35f939b236b957e61c7
cet9.dev <cet9dev@gmail.com>
2021-07-29 09:32:35 -0300

    inscripcion

---
+++ src/components/Inscripcion.vue
@@ -0,0 +1,25 @@
+<template>
+ <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Nombre y Apellido</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nombre del alumno">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">DNI</label>
+  <textarea class="form-control" id="Num." rows="3"></textarea>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Curso y Division</label>
+  <input type="email" class="form-control" id="" placeholder="Ingresar">
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'Inscripcion',
+  props: {
+    msg: String
+  }
+}
+</script>
