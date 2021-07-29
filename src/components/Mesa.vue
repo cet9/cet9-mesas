73311219df3268682d5bcf6905f5ecb60991e5dc
cet9 <cet9dev@gmail.com>
2021-07-29 10:33:56 -0300

    subo avance mesa

--- src/components/Mesa.vue
+++ src/components/Mesa.vue
@@ -1,39 +1,42 @@
 <template>
 
 <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Materia a rendir</label>
+  <label for="exampleFormControlInput1" class="form-label">Materia a rendir:</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=" Ej lengua y Literatura">
 </div>
 
  <div class="mb-3">
-  <input class="form-control" type="text" placeholder="Curso" aria-label="default input example">
+  <label for="exampleFormControlInput1" class="form-label">Curso:</label>
+  <input class="form-control" type="text" placeholder="1" aria-label="default input example">
 </div>
 
  <div class="mb-3">
-  <input class="form-control" type="text" placeholder="Division" aria-label="default input example">
+ <label for="exampleFormControlInput1" class="form-label">Division:</label>
+  <input class="form-control" type="text" placeholder="3°" aria-label="default input example">
  </div>
 
  <div class="mb-3">
-  <input class="form-control" type="text" placeholder="Ciclo" aria-label="default input example">
+ <label for="exampleFormControlInput1" class="form-label">Ciclo:</label>
+  <input class="form-control" type="text" placeholder="Ciclo superior o primario" aria-label="default input example">
  </div>
- 
+
  <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Fecha</label>
+  <label for="exampleFormControlInput1" class="form-label">Fecha:</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Fecha a rendir">
 </div>
 
 <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Hora</label>
+  <label for="exampleFormControlInput1" class="form-label">Hora:</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Hora a rendir">
 </div>
 
 <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Docente</label>
+  <label for="exampleFormControlInput1" class="form-label">Docente:</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nonbre del docente">
 </div>
 
 <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Aula</label>
+  <label for="exampleFormControlInput1" class="form-label">Aula:</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=" Ej Aula B-1">
 </div>
 
