e6fb50c2c8fadddbd340ebea7fcda70bdc82e882
cet9.dev <cet9dev@gmail.com>
2021-07-29 09:32:03 -0300

    correcciones

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <Menu/>
   <HelloWorld msg="Inscripción a mesas"/>
-  
+  <Docente/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Menu from './components/Menu.vue'
+import Docente from './components/Docente.vue'
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Menu
+    Menu,
+    Docente
   }
 }
 </script>
