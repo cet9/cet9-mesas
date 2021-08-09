a1385a54e2cba4589f14785f8b8b98a4678c6cf7
cet9.dev <cet9dev@gmail.com>
2021-08-09 10:03:11 -0300

    agrego margen superior

--- src/components/MesaForm.vue
+++ src/components/MesaForm.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container">
+<div class="container mt-3">
 <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Materia a rendir:</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=" Ej lengua y Literatura">
@@ -32,7 +32,7 @@
 
 <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Docente:</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nonbre del docente">
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Nombre del docente">
 </div>
 
 <div class="mb-3">
