a98f5f038d1c789306576ad6d4611943f62dd703
cet9.dev <cet9dev@gmail.com>
2021-08-09 10:32:35 -0300

    color del boton

--- src/components/MesaForm.vue
+++ src/components/MesaForm.vue
@@ -46,7 +46,7 @@
 </div>
 
 <div class= "mb-3">
-<button type="button" class="btn btn-success">Guardar</button>
+<button type="button" class="btn btn-outline-primary">Guardar</button>
 </div>
 </div>
 </div>
