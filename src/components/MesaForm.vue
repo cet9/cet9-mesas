31a3e36465e385d57261ef98dbd2e72bda6ce944
cet9.dev <cet9dev@gmail.com>
2021-08-09 09:54:55 -0300

    se agrega container

--- src/components/MesaForm.vue
+++ src/components/MesaForm.vue
@@ -1,5 +1,5 @@
 <template>
-
+<div class="container">
 <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Materia a rendir:</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=" Ej lengua y Literatura">
@@ -40,6 +40,11 @@
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=" Ej Aula B-1">
 </div>
 
+<div class= "mb-3">
+<button type="button" class="btn btn-success">Guardar</button>
+</div>
+
+</div>
 
 
 </template>
