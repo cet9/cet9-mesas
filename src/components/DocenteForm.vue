6dc4883fed8554abc670537426d48f01baf1c5c3
cet9 <cet9dev@gmail.com>
2021-08-09 10:51:36 -0300

    Correción de formulario

--- src/components/DocenteForm.vue
+++ src/components/DocenteForm.vue
@@ -1,54 +1,51 @@
 <template>
 
 
-<div class="container">
+<div class="container mt-3">
 
-<form class="row g-3">
-  <div class="col-md-6">
+<div class="card border-primary mb-3">
+  <div class="card-header">Alta docente</div>
+  <div class="card-body">
+      <div class="mb-3">
     <label for="inputEmail4" class="form-label">Nombre</label>
-    <input type="email" class="form-control" id="inputEmail4">
+    <input type="email" class="form-control" id="inputEmail4" placeholder= "Ingrese nombre">
   </div>
-  <div class="col-md-6">
+  <div class="mb-3">
     <label for="inputPassword4" class="form-label">Apellido</label>
-    <input type="password" class="form-control" id="inputPassword4">
+    <input type="text" class="form-control" id="inputPassword4" placeholder= "Ingrese apellido">
   </div>
-  <div class="col-12">
+  <div class="mb-3">
     <label for="inputAddress" class="form-label">DNI</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="22000000">
+    <input type="number" class="form-control" id="inputAddress" placeholder="ej: 22000000">
   </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label">Gmail</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="name@example.com">
+  <div class="mb-3">
+    <label for="inputAddress2" class="form-label">Email</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="ej: name@example.com">
   </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">Numero Celular</label>
-    <input type="text" class="form-control" id="inputCity">
+  <div class="mb-3">
+    <label for="inputCity" class="form-label">Número Celular</label>
+    <input type="number" class="form-control" id="inputCity" placeholder= "ej: 299-4222555">
   </div>
-  <div class="col-md-4">
-
-
-
-
-
-
  
 
-
-
-  </div>
-
-  <div class="col-12">
+  <div class="mb-3">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" id="gridCheck">
       <label class="form-check-label" for="gridCheck">
-        Guardar mi Informacion
+        Guardar información
       </label>
     </div>
   </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Registarse</button>
+  <div class="mb-3">
+    <button type="submit" class="btn btn-outline-primary">Registrarse</button>
   </div>
-</form>
+  </div>
+</div>
+
+
+
+
+  
 
 </div>
 
