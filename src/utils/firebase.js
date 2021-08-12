2604c0d699fcaffdcd91fa39d130e88fc4785b2f
cet9 <cet9dev@gmail.com>
2021-08-12 09:20:50 -0300

    Correción firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,14 +1,18 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA4ZFrAeVYOQkoqrpG00RKfJeRAP_gM5IU",
-    authDomain: "cet9-mesas.firebaseapp.com",
-    projectId: "cet9-mesas",
-    storageBucket: "cet9-mesas.appspot.com",
-    messagingSenderId: "759566903252",
-    appId: "1:759566903252:web:b67313b3d57bfeee9fd841",
-    measurementId: "G-LDMPTSEWJ8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  apiKey: "AIzaSyA4ZFrAeVYOQkoqrpG00RKfJeRAP_gM5IU",
+  authDomain: "cet9-mesas.firebaseapp.com",
+  projectId: "cet9-mesas",
+  storageBucket: "cet9-mesas.appspot.com",
+  messagingSenderId: "759566903252",
+  appId: "1:759566903252:web:b67313b3d57bfeee9fd841",
+  measurementId: "G-LDMPTSEWJ8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export { db }
--- src/views/Inscripcion.vue
+++ src/views/Inscripcion.vue
@@ -1,15 +1,44 @@
 <template>
     <InscripcionForm/>
+    <ul class="list-group">
+      <li v-for="u in data" class="list-group-item" :key="u.id">{{ u.username}}</li>
+    </ul>
 </template>
 
 <script>
 // @ is an alias to /src
 import InscripcionForm from '@/components/InscripcionForm.vue'
 
+import { db } from "@/utils/firebase"
+
 export default {
   name: 'Inscripcion',
   components: {
     InscripcionForm
+  },
+
+  data(){
+    return {
+        data:[]
+    }
+  },
+
+  methods: {
+      async obtenerUsuarios()
+      {
+          const usuarios =  await db.collection("usuario").get()
+
+          const u = usuarios.docs.map( doc => {
+              return doc.data();
+          });
+
+          this.data = u;
+      },
+
+  },
+
+  mounted() {
+    this.obtenerUsuarios()
   }
 }
 </script>
