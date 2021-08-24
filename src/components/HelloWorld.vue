d1930319cecb44d784afd22aab922cc8c1beb6ca
cet9.dev <cet9dev@gmail.com>
2021-08-24 09:33:59 -0300

    Subo cambio Mesas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,82 @@
 <template>
- <div class="container mt-3">
-   <div class="form-group" style="max-width: 20rem;">>
-      <label for="exampleSelect1" class="form-label mt-4">Año:</label>
-      <select class="form-select" id="exampleSelect1">
-        <option>1° C.B.</option>
-        <option>2° C.B.</option>
-        <option>1° C.S.T.E</option>
-        <option>1° C.S.T.M</option>
-        <option>2° C.S.T.E</option>
-        <option>2° C.S.T.M</option>
-        <option>3° C.S.T.E</option>
-        <option>3° C.S.T.M</option>
-        <option>4° C.S.T.E</option>
-        <option>4° C.S.T.M</option>
-      </select>
+
+<div class="container mt-3">
+
+      <div class="form-group" style="max-width: 20rem;">
+          <label for="exampleSelect1" class="form-label mt-4">Año:</label>
+          <select class="form-select" id="exampleSelect1">
+            <option>1° C.B.</option>
+            <option>2° C.B.</option>
+            <option>1° C.S.T.E</option>
+            <option>1° C.S.T.M</option>
+            <option>2° C.S.T.E</option>
+            <option>2° C.S.T.M</option>
+            <option>3° C.S.T.E</option>
+            <option>3° C.S.T.M</option>
+            <option>4° C.S.T.E</option>
+            <option>4° C.S.T.M</option>
+          </select>
+        </div>
+
+    
+    <div class="row">
+
+        <div v-for="m of datos" v-bind:key="m.curso" class="col-12 col-md-3 col-lg-4">
+
+            <div class="card border-primary mb-3 mt-3" >
+              <div class="card-header text-center">Fecha:{{m.fecha}} Horario:{{m.horario}}</div>
+              <div class="card-body">
+                <h4 class="card-title">{{m.materia}}</h4>
+                <p class="card-text text-start text-primary">
+                  Docente:{{m.docente}} <br>
+                  Aula:{{m.aula}}
+                </p>
+                <div class="d-grid gap-2">
+                <router-link class="btn btn-primary" style="color:white" to="/inscripcion">inscribirse</router-link>
+                </div>
+              </div>
+            </div>
+
+
+
+        </div>
+
     </div>
-  <div class="card border-primary mb-3 mt-3" style="max-width: 20rem;">
-  <div class="card-header text-center">Fecha:10/05/23 Horario:9:20</div>
-  <div class="card-body">
-    <h4 class="card-title">Electrotecnia II</h4>
-    <p class="card-text text-start text-primary">
-      Docente:Pablo Sanchez <br>
-      Aula:P.A. 3
-    </p>
-    <div class="d-grid gap-2">
-    <router-link class="btn btn-primary" style="color:white" to="/inscripcion">inscribirse</router-link>
-    </div>
-   </div>
+
 </div>
-  
-</div>
+
+
+
 
  
 </template>
 
 <script>
+import {db} from "@/utils/firebase"
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data (){
+    return{
+      datos:[]
+    }
+  },
+  
+  methods: {
+    async listar(){
+      const mesas = await db.collection("Mesas").get()
+
+        this.datos = mesas.docs.map(doc=>{
+              return doc.data()
+
+        });
+
+    }
+  
+    
+  
+  },
+  mounted(){
+    this.listar()
   }
 }
 </script>
